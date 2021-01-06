32c96614f0958390e8328755742c1a8f9caae5fe
dragos001 <38700214+dragos001@users.noreply.github.com>
2021-01-07 00:56:37 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,8 +2,47 @@ import ply.lex as lex
 import ply.yacc as yacc
 import sys
 
+tokens = [
 
+    'INT',
+    'FLOAT',
+    'NAME',
+    'PLUS',
+    'MINUS',
+    'DIVIDE',
+    'MULTIPLY',
+    'EQUALS'
 
+]
+
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_EQUALS = r'\='
+
+t_ignore = r' '
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = 'NAME'
+    return t
+
+def t_error(t):
+    print("Illegal characters!")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
 
 
 
