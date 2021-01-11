8cdbd90621c66bad298915fd9704e68302826fc5
dragos001 <38700214+dragos001@users.noreply.github.com>
2021-01-11 20:40:16 +0200

    Update calculator.py
    Modificare cod (2)

--- calculator.py
+++ calculator.py
@@ -1,90 +1,40 @@
-import re
-from operator import itemgetter
-from collections import deque
-from jinja2.exceptions import TemplateSyntaxError
-from jinja2.utils import LRUCache
+import math
 
-_lexer_cache = LRUCache(50)
-
-whitespace_re = re.compile(r'\s+', re.U)
-string_re = re.compile(r"('([^'\\]*(?:\\.[^'\\]*)*)'"
-                       r'|"([^"\\]*(?:\\.[^"\\]*)*)")', re.S)
-integer_re = re.compile(r'\d+')
-name_re = re.compile(r'\b[a-zA-Z_][a-zA-Z0-9_]*\b')
-float_re = re.compile(r'(?<!\.)\d+\.\d+')
-newline_re = re.compile(r'(\r\n|\r|\n)')
 
 operators = {
-    '+':            'add',
-    '-':            'sub',
-    '/':            'div',
-    '//':           'floordiv',
-    '*':            'mul',
-    '%':            'mod',
-    '**':           'pow',
-    '~':            'tilde',
-    '[':            'lbracket',
-    ']':            'rbracket',
-    '(':            'lparen',
-    ')':            'rparen',
-    '{':            'lbrace',
-    '}':            'rbrace',
-    '==':           'eq',
-    '!=':           'ne',
-    '>':            'gt',
-    '>=':           'gteq',
-    '<':            'lt',
-    '<=':           'lteq',
-    '=':            'assign',
-    '.':            'dot',
-    ':':            'colon',
-    '|':            'pipe',
-    ',':            'comma',
-    ';':            'semicolon'
+    '+': (1, lambda a, b: a + b),
+    '-': (1, lambda a, b: a - b),
+    '*': (2, lambda a, b: a * b),
+    '/': (2, lambda a, b: a / b),
+    '%': (2, lambda a, b: a % b),
+    '^': (3, lambda a, b: a ** b)
 }
 
-reverse_operators = dict([(v, k) for k, v in operators.iteritems()])
-assert len(operators) == len(reverse_operators), 'operators dropped'
-operator_re = re.compile('(%s)' % '|'.join(re.escape(x) for x in
-                         sorted(operators, key=lambda x: -len(x))))
 
-def count_newlines(value)
- return len(newline_re.findall(value))
+functions = {
+    'rad': (1, lambda a: math.sqrt(a)),
+    'sin': (1, lambda a: math.sin(a)),
+    'asin': (1, lambda a: math.asin(a)),
+    'cos': (1, lambda a: math.cos(a)),
+    'acos': (1, lambda a: math.acos(a)),
+    'tan': (1, lambda a: math.tan(a)),
+    'atan': (1, lambda a: math.atan(a)),
+    'log': (1, lambda a: math.log10(a)),
+    'ln': (1, lambda a: math.log(a))
+}
 
- class Failure(object)
-  
-def __init__(self, message, cls=TemplateSyntaxError):
-        self.message = message
-        self.error_class = cls
 
-    def __call__(self, lineno, filename):
-        raise self.error_class(self.message, lineno, filename)
+def solve(raw_exp):
+    """Return the solution of the provided raw expression"""
+    postfix_exp = _to_postfix(_tokenize_expression(raw_exp))
+    number_stack = []
 
-class Token(tuple)
-
- _slots_ = ()
-    lineno, type, value = (property(itemgetter(x)) for x in range(3))
-
-    def __new__(cls, lineno, type, value):
-        return tuple.__new__(cls, (lineno, intern(str(type)), value))
-
-    def __str__(self):
-        if self.type in reverse_operators:
-            return reverse_operators[self.type]
-        elif self.type is 'name':
-            return self.value
-        return self.type
-    
-    def test(self, expr)
-     
-    if self.type == expr:
-            return True
-        elif ':' in expr:
-            return expr.split(':', 1) == [self.type, self.value]
-        return False
-
-     if self.type == expr:
-            return True
-        elif ':' in expr:
-            return expr.split(':', 1) == [self.type, self.value]
-        return False
+    for token in postfix_exp:
+        if _is_number(token):
+            number_stack.append(float(token))
+        elif token in operators:
+            temp = number_stack.pop()
+            number_stack.append(operators[token][1](number_stack.pop(), temp))
+        elif token in functions:
+            if functions[token][0] == 1:
+                number_stack.append(functions[token][1](number_stack.pop()))
